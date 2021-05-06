d6019a61773195b36f37430a70caadd8245a2db9
c-noblet <c.noblet.dev@gmail.com>
2021-05-06 11:14:55 +0200

    Ajout de la gestion des articles

--- pages/admin/form-artist.vue
+++ pages/admin/form-artist.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <form
-      @submit="submitForm($event)"
       class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      @submit="submitForm($event)"
     >
       <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
           Nom
         </label>
         <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="name"
+          v-model="artist.name"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           placeholder="Nom"
           required
-          v-model="artist.name"
         >
       </div>
       <div class="mb-4">
@@ -22,41 +22,40 @@
           Description
         </label>
         <textarea
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="description"
+          v-model="artist.description"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="description"
           placeholder="Description"
           required
-          v-model="artist.description"
-        >
-        </textarea>
+        />
       </div>
       <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="origin">Orgine</label>
         <input
+          id="origin"
+          v-model="artist.origin"
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           name="origin"
-          id="origin"
           required
-          v-model="artist.origin"
         >
       </div>
       <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="avatar">Avatar (url)</label>
         <input
+          id="avatar"
+          v-model="artist.avatar"
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="url"
           name="avatar"
-          id="avatar"
           required
-          v-model="artist.avatar"
         >
       </div>
       <div class="inline-block relative w-64">
         <select
-          class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
           v-model="artist.genreId"
+          class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
         >
           <option
             v-for="genre in genres"
@@ -68,7 +67,7 @@
           </option>
         </select>
         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
         </div>
       </div>
       <div class="flex items-center justify-between">
@@ -97,6 +96,7 @@ export default {
   },
   created () {
     this.unsubscribe = this.$store.subscribe((mutations) => {
+      console.log(mutations.type, mutations.payload)
       if (mutations.type === 'artist/ARTIST') {
         this.artist = mutations.payload
       } else if (mutations.type === 'genre/GENRES') {
@@ -110,6 +110,9 @@ export default {
     if (this.$route.params.artistId) {
       this.fetchArtist(this.$route.params.artistId)
     }
+  },
+  beforeDestroy () {
+    this.unsubscribe()
   },
   methods: {
     submitForm (event) {
@@ -134,9 +137,6 @@ export default {
       this.artist.id ? this.$store.dispatch('flashMessage/addSuccessMessage', 'Artiste/Groupe modifié avec succès !') : this.$store.dispatch('flashMessage/addSuccessMessage', 'Artiste/Groupe ajouté avec succès !')
       this.$router.push('/admin/artists')
     }
-  },
-  beforeDestroy () {
-    this.unsubscribe()
   }
 }
 </script>
---
+++ pages/admin/news.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Gestion des news</h1>
+    <NuxtLink to="/admin/form-article">
+      Ajouter
+    </NuxtLink>
+    <ArticleTable :articles="articles" />
+  </div>
+</template>
+<script>
+import ArticleTable from '@/components/Articles/ArticleTable'
+export default {
+  name: 'News',
+  components: {
+    ArticleTable
+  },
+  data () {
+    return {
+      articles: []
+    }
+  }
+}
+</script>
