293b84b9a54c79ae7de801a8b31f12f38946fd88
VGimonnet <valentin.gimonnet18@gmail.com>
2021-05-11 12:05:59 +0200

    Modification style component artistCard

--- components/Artist/ArtistCard.vue
+++ components/Artist/ArtistCard.vue
@@ -2,7 +2,7 @@
   <NuxtLink :to="{name: 'artist-id', params: {id: artist.id }}" class="artist-card">
     <div class="flex flex-col justify-center items-center">
       <img :src="artist.avatar" alt="Avatar de l'artiste/groupe">
-      <h2>{{ artist.name }}</h2>
+      <h2 class="text-center text-gray-200 text-semibold text-2xl">{{ artist.name }}</h2>
     </div>
   </NuxtLink>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col justify-center items-center mb-20">
       <h1 class="text-center text-4xl font-bold pt-1 pb-2 px-8 border-t-4 border-b-4 border-gray-100">Musik-News</h1>
     </div>
-    <section class="my-5">
+    <section class="mb-20">
       <h2 class="flex items-center py-1 px-8 border-l-4 border-gray-100 text-3xl font-semibold mb-3">Nouveautés</h2>
       <ul class="flex flex-col md:flex-row mb-5">
         <li v-for="article in articles" :key="article.id" class="m-5 w-3/6">
@@ -14,7 +14,7 @@
         Toutes les nouveautés
       </nuxt-link>
     </section>
-    <section class="my-5">
+    <section class="mb-20">
       <h2 class="flex items-center py-1 px-8 border-l-4 border-gray-100 text-3xl font-semibold mb-3">Derniers albums</h2>
       <ul class="flex flex-row flew-wrap justify-center items-center">
         <li v-for="album in albums" :key="album.id" :album="album" class="album flex flex-col justify-center items-center">
@@ -30,9 +30,9 @@
         </li>
       </ul>
     </section>
-    <section class="my-5">
+    <section class="">
       <h2 class="flex items-center py-1 px-8 border-l-4 border-gray-100 text-3xl font-semibold mb-3">Top Artistes</h2>
-      <div class="flex flex-row flex-wrap justify-around items-center">
+      <div class="flex flex-row flex-wrap justify-around items-center mb-5">
         <artist-card v-for="artist in artists" :key="artist.id" :artist="artist" />
       </div>
       <nuxt-link to="/artists" class="btn btn-primary ml-auto table">
